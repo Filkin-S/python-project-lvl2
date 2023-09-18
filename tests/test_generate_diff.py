2448cbef03330a926d6e72b4e6f73ca2ab6ef617
Filkin-S <fsp1988@yandex.ru>
2023-09-18 23:03:29 +0300

    changes import

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,4 +1,4 @@
-from gendiff.engine import generate_diff
+from gendiff import engine
 from gendiff.formatters import plain, stylish
 
 
@@ -6,67 +6,67 @@ def test_generate_diff_nested_json():
     with open('tests/fixtures/result_nested.txt') as f:
         expected = f.read()
 
-    assert generate_diff('tests/fixtures/file1_nested.json',
-                         'tests/fixtures/file2_nested.json',
-                         stylish.make_stylish
-                         ) == expected
+    assert engine.generate_diff('tests/fixtures/file1_nested.json',
+                                'tests/fixtures/file2_nested.json',
+                                stylish.make_stylish
+                                ) == expected
 
 
 def test_generate_diff_nested_yml():
     with open('tests/fixtures/result_nested.txt') as f:
         expected = f.read()
 
-    assert generate_diff('tests/fixtures/file1_nested.yml',
-                         'tests/fixtures/file2_nested.yml',
-                         stylish.make_stylish
-                         ) == expected
+    assert engine.generate_diff('tests/fixtures/file1_nested.yml',
+                                'tests/fixtures/file2_nested.yml',
+                                stylish.make_stylish
+                                ) == expected
 
 
 def test_generate_diff_nested_plain():
     with open('tests/fixtures/result_nested_plain.txt') as f:
         expected = f.read()
 
-    assert generate_diff('tests/fixtures/file1_nested.json',
-                         'tests/fixtures/file2_nested.json',
-                         plain.make_plain
-                         ) == expected
+    assert engine.generate_diff('tests/fixtures/file1_nested.json',
+                                'tests/fixtures/file2_nested.json',
+                                plain.make_plain
+                                ) == expected
 
 
 def test_generate_diff_hexlet_json_stylish():
     with open('tests/fixtures/result_stylish') as f:
         expected = f.read()
 
-    assert generate_diff('tests/fixtures/file1.json',
-                         'tests/fixtures/file2.json',
-                         stylish.make_stylish
-                         ) == expected
+    assert engine.generate_diff('tests/fixtures/file1.json',
+                                'tests/fixtures/file2.json',
+                                stylish.make_stylish
+                                ) == expected
 
 
 def test_generate_diff_hexlet_yaml_stylish():
     with open('tests/fixtures/result_stylish') as f:
         expected = f.read()
 
-    assert generate_diff('tests/fixtures/file1.yml',
-                         'tests/fixtures/file2.yml',
-                         stylish.make_stylish
-                         ) == expected
+    assert engine.generate_diff('tests/fixtures/file1.yml',
+                                'tests/fixtures/file2.yml',
+                                stylish.make_stylish
+                                ) == expected
 
 
 def test_generate_diff_hexlet_json_plain():
     with open('tests/fixtures/result_plain') as f:
         expected = f.read()
 
-    assert generate_diff('tests/fixtures/file1.json',
-                         'tests/fixtures/file2.json',
-                         plain.make_plain
-                         ) == expected
+    assert engine.generate_diff('tests/fixtures/file1.json',
+                                'tests/fixtures/file2.json',
+                                plain.make_plain
+                                ) == expected
 
 
 def test_generate_diff_hexlet_yaml_plain():
     with open('tests/fixtures/result_plain') as f:
         expected = f.read()
 
-    assert generate_diff('tests/fixtures/file1.yml',
-                         'tests/fixtures/file2.yml',
-                         plain.make_plain
-                         ) == expected
+    assert engine.generate_diff('tests/fixtures/file1.yml',
+                                'tests/fixtures/file2.yml',
+                                plain.make_plain
+                                ) == expected
